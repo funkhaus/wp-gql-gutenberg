4641717b1a471042c5681b53f26e82dd1fa7af57
rsm0128 <rsm0128@hotmail.com>
2021-04-20 05:38:02 +0900

    added backgroundColorCode, textColorCode, fontSizeValue

--- src/Blocks/Block.php
+++ src/Blocks/Block.php
@@ -125,27 +125,6 @@ class Block implements ArrayAccess {
 		$types = [$block_type['attributes']];
 		$attributes = $data['attrs'];
 
-		if ( isset( $attributes['style'] ) ) {
-
-			// read textColor, backgroundColor from style
-			if ( isset( $attributes['style']['color'] ) ) {
-				if ( isset( $attributes['style']['color']['text'] ) ) {
-					$attributes['textColor'] = $attributes['style']['color']['text'];
-				}
-				if ( isset( $attributes['style']['color']['background'] ) ) {
-					$attributes['backgroundColor'] = $attributes['style']['color']['background'];
-				}
-			}
-
-			// read fontSize from style
-			if ( isset( $attributes['style']['typography'] ) && isset( $attributes['style']['typography']['fontSize'] ) ) {
-				$attributes['fontSize'] = $attributes['style']['typography']['fontSize'];
-			}
-
-			// cast style from array to object.
-			$attributes['style'] = (object)$attributes['style'];
-		}
-
 		foreach ($block_type['deprecated'] ?? [] as $deprecated) {
 			if (!empty($deprecated['attributes'])) {
 				$types[] = $deprecated['attributes'];
@@ -197,7 +176,7 @@ class Block implements ArrayAccess {
 
 		$result = self::parse_attributes($data, $this->blockType);
 
-		$this->attributes = apply_filters( 'ggb_attributes_' . $this->name, $result['attributes'] );
+		$this->attributes = apply_filters( 'ggb_attributes' , $result['attributes'], $this->name );
 		$this->attributesType = $result['type'];
 	}
 
